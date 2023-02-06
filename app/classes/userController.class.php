605cb51fad579ea30dbdba016787e8c4e0301856
cogsincogs <jamiewthomas13@gmail.com>
2023-02-06 21:42:59 +0000

    Refactor userController to accept password hashes and add getUser functions

--- app/classes/userController.class.php
+++ app/classes/userController.class.php
@@ -3,22 +3,43 @@
 include $_SERVER['DOCUMENT_ROOT'] . '/classes/dbh.class.php';
 
 class UserController extends Dbh {
-    public function setUser($un, $pw){
-        $sql = "INSERT INTO users (username, password) VALUES (:un, :pw);";
+    public function setUser($un, $em, $pw){
+        $sql = "INSERT INTO users (name, email, password_hash) VALUES (:un, :em, :pw);";
         $stmt = $this->connect()->prepare($sql);
-        $stmt->execute([
-            'un' => $un,
-            'pw' => $pw
-        ]);
+        try {
+            $stmt->execute([
+                'un' => $un,
+                'em' => $em,
+                'pw' => $pw
+            ]);
+        } catch (PDOException $e) {
+            if (str_contains($e->getMessage(), "1062 Duplicate entry")) { 
+                die("Duplicate email.");
+            };
+        }
     }
 
-    public function checkUnique($un){
-        $sql = "SELECT * FROM users WHERE username = '$un'";
+    // These getUserBy methods can be refactored with optional parameters so it's only defined once
+    // and can be called by getUser(email: $em) for example
+    public function getUserByEmail($em) {
+        $sql = "SELECT * FROM users WHERE email = '$em'";
         $stmt = $this->connect()->prepare($sql);
-        $stmt->execute();
-        if ($stmt->fetch() != '') {
-            return false;
+        try {
+            $stmt->execute();
+        } catch (PDOException $e) {
+            die($e);
         }
-        return true;
+        return $stmt->fetch();
+    }
+
+    public function getUserByID($id) {
+        $sql = "SELECT * FROM users WHERE id = '$id'";
+        $stmt = $this->connect()->prepare($sql);
+        try {
+            $stmt->execute();
+        } catch (PDOException $e) {
+            die($e);
+        }
+        return $stmt->fetch();
     }
 }
