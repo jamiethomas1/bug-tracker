3710dd3d1034ec2f736e60384903c906cdbf4d80
cogsincogs <jamiewthomas13@gmail.com>
2023-02-09 14:30:04 +0000

    Remove text 'index.php' from redirect link to clean up URL

--- app/login/index.php
+++ app/login/index.php
@@ -20,7 +20,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             session_start();
             session_regenerate_id();
             $_SESSION["user_id"] = $user["id"];
-            header("Location: ../index.php");
+            header("Location: ../");
             exit;
         }
     }
