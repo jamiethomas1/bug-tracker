a8af11691e8be94d8321e2186e9518026aba9698
cogsincogs <jamiewthomas13@gmail.com>
2023-02-27 14:41:54 +0000

    Add POST & default cases for Collection Requests

--- app/api/src/ProductController.php
+++ app/api/src/ProductController.php
@@ -23,6 +23,46 @@ class ProductController {
             case "GET":
                 echo json_encode($this->gateway->getAll());
                 break;
+            case "POST":
+                $data = (array) json_decode(file_get_contents("php://input"), true);
+
+                $errors = $this->getValidationErrors($data);
+
+                if (!empty($errors)) {
+                    http_response_code(422);
+                    echo json_encode(["errors" => $errors]);
+                    break;
+                }
+
+                $id = $this->gateway->create($data);
+                http_response_code(201);
+                echo json_encode([
+                    "message" => "User created",
+                    "id" => $id
+                ]);
+                break;
+            default:
+                http_response_code(405);
+                header("Allow: GET, POST");
         }
+    }
+
+    private function getValidationErrors(array $data): array {
+        $errors = [];
+
+        if (empty($data["email"])) {
+            $errors[] = "email is required";
+        }
+        if (empty($data["name"])) {
+            $errors[] = "name is required";
+        }
+        if (empty($data["password_hash"])) {
+            $errors[] = "password_hash is required";
+        }
+        if (empty($data["userID"])) {
+            $errors[] = "userID is required";
+        }
+
+        return $errors;
     }
 }
