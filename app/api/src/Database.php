ec622325e2d48f072b6a5781e0ae165470666a4d
cogsincogs <jamiewthomas13@gmail.com>
2023-02-27 14:08:11 +0000

    Stop PDO from stringifying integer values

--- app/api/src/Database.php
+++ app/api/src/Database.php
@@ -4,6 +4,12 @@ include_once("../classes/dbh.class.php");
 
 class Database extends Dbh {
     public function getConnection() {
-        return $this->connect();
+
+        $options = [
+            PDO::ATTR_EMULATE_PREPARES => false,
+            PDO::ATTR_STRINGIFY_FETCHES => false
+        ];
+
+        return $this->connect($options);
     }
 }
