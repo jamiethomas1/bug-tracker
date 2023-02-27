047c1e188051dfb4df8f91176a81e43e79568e89
cogsincogs <jamiewthomas13@gmail.com>
2023-02-27 14:39:55 +0000

    Add error handler above exception handler

--- app/api/index.php
+++ app/api/index.php
@@ -9,6 +9,7 @@ spl_autoload_register(function ($class) {
     require __DIR__ . "/src/$class.php";
 });
 
+set_error_handler("ErrorHandler::handleError");
 set_exception_handler("ErrorHandler::handleException");
 
 header("Content-Type: application/json; charset=UTF-8");
