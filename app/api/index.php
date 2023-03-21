ebcd4d33eadaa23a696a8fafbe1a4b8f65746058
cogsincogs <jamiewthomas13@gmail.com>
2023-03-21 19:55:45 +0000

    Set up tickets endpoint#

--- app/api/index.php
+++ app/api/index.php
@@ -37,6 +37,11 @@ switch ($parts[2]) {
         $controller = new ProjController($gateway);
         $controller->processRequest($_SERVER["REQUEST_METHOD"], $id);
         break;
+    case "tickets":
+        $gateway = new TicketGateway($dbh);
+        $controller = new TicketController($gateway);
+        $controller->processRequest($_SERVER["REQUEST_METHOD"], $id);
+        break;
     default:
         http_response_code(404);
         exit;
---
+++ app/api/src/TicketGateway.php
@@ -0,0 +1,82 @@
+<?php
+
+// Is this file necessary or can I just reference the userController class?
+// Also how can I get rid of the ugly HTML errors?
+class TicketGateway {
+    private PDO $conn;
+
+    public function __construct(Database $database)
+    {
+        $this->conn = $database->getConnection();
+    }
+
+    public function getAll(): array {
+        $sql = "SELECT * FROM tickets";
+        $stmt = $this->conn->query($sql);
+        $data = [];
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $data[] = $row;
+        }
+        return $data;
+    }
+
+    public function create(array $data) {
+      $sql = "INSERT INTO tickets (name, body, ownerID, orgID, projID, ticketID) 
+        VALUES (:name, :body, :ownerID, :orgID, :projID, :ticketID)";
+        $stmt = $this->conn->prepare($sql);
+
+        try {
+            $stmt->execute([
+                'name' => $data["name"],
+                'body' => $data["body"],
+                'ownerID' => $data["ownerID"],
+                'orgID' => $data["orgID"],
+                'projID' => $data["projID"],
+                'ticketID' => $data["ticketID"]
+            ]);
+        } catch (PDOException $e) {
+            if (str_contains($e->getMessage(), "1062 Duplicate entry")) { 
+                die("Duplicate organisation ID.");
+            };
+        }
+
+        return $this->conn->lastInsertId();
+    }
+
+    public function get(string $id): array | false {
+        $sql = "SELECT * FROM tickets WHERE ticketID = :id";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([
+            'id' => $id
+        ]);
+        $data = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $data;
+    }
+
+    public function update(array $current, array $new): int {
+      $sql = "UPDATE tickets 
+        SET name = :name, body = :body, ownerID = :ownerID, orgID = :orgID, projID = :projID, ticketID = :ticketID 
+        WHERE id = :id";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindValue(":name", $new['name'] ?? $current['name'], PDO::PARAM_STR);
+        $stmt->bindValue(":body", $new['body'] ?? $current['body'], PDO::PARAM_STR);
+        $stmt->bindValue(":ownerID", $new['ownerID'] ?? $current['ownerID'], PDO::PARAM_STR);
+        $stmt->bindValue(":orgID", $new['orgID'] ?? $current['orgID'], PDO::PARAM_STR);
+        $stmt->bindValue(":projID", $new['projID'] ?? $current['projID'], PDO::PARAM_STR);
+        $stmt->bindValue(":ticketID", $new['ticketID'] ?? $current['ticketID'], PDO::PARAM_STR);
+        
+        $stmt->bindValue(":id", $current['id'], PDO::PARAM_INT);
+
+        $stmt->execute();
+        return $stmt->rowCount();
+    }
+
+    public function delete(string $id): int {
+        $sql = "DELETE FROM tickets WHERE ticketID = :id";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([
+            'id' => $id
+        ]);
+        return $stmt->rowCount();
+    }
+}
