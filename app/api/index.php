5cbf6d221dedea1f6c27d762258bbc0cc3a0f658
cogsincogs <jamiewthomas13@gmail.com>
2023-04-21 01:18:48 +0100

    Fix logic for determining beginning of req data

--- app/api/index.php
+++ app/api/index.php
@@ -6,6 +6,9 @@ ini_set("html_errors", 0);
 
 require "/vendor/autoload.php";
 
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
 // Look at using something like Composer for autoloading
 spl_autoload_register(function ($class) {
     require __DIR__ . "/src/$class.php";
@@ -21,7 +24,18 @@ $parts = explode("/", $_SERVER['REQUEST_URI']);
 
 $dbh = new Database;
 
-$id = end($parts) ?? null;
+if ($parts[2] == "auth") {
+    $id = $parts[4] ?? null;
+} else {
+    $id = $parts[3] ?? null;
+}
+
+$key = getenv("JWT_SIGNATURE_KEY");
+if (array_key_exists("HTTP_AUTHORIZATION", $_SERVER)) {
+    $jwt = explode(" ", $_SERVER["HTTP_AUTHORIZATION"])[1];
+    $decoded = JWT::decode($jwt, new Key($key, 'HS256'));
+    print_r($decoded);
+}
 
 switch ($parts[2]) {
     case "auth":
@@ -29,7 +43,7 @@ switch ($parts[2]) {
             case "login":
                 $gateway = new LoginGateway($dbh);
                 $controller = new LoginController($gateway);
-                $controller->processRequest($_SERVER["REQUEST_METHOD"], null);
+                $controller->processRequest($_SERVER["REQUEST_METHOD"]);
                 break;
             default:
                 http_response_code(404);
